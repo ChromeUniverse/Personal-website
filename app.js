f5066bd00779848f19bd604c28ab4a32268a8c39
Lucca <omnichrome2@gmail.com>
2021-05-27 12:12:58 -0300

    Added routing system and improved template engine

--- app.js
+++ app.js
@@ -13,23 +13,53 @@ const marked = require("marked");
 // load page with file name
 function get_page(file_name) {  
   // load template
-  let html = fs.readFileSync(__dirname + '/templates/base.html').toString();
-  // load content
-  let text = fs.readFileSync(__dirname + '/posts/' + file_name + '.md').toString();
-  // convert markdown -> HTML
-  text = marked(text);
-  // add content to template
-  html = html.replace('CONTENTGOESHERE', text);
-  return html;
+  let html = fs.readFileSync(__dirname + '/public/templates/base.html').toString();
+  let text;
+
+  try {
+    // load content   
+    text = fs.readFileSync(__dirname + '/public/posts/' + file_name + '.md').toString();
+    // convert markdown -> HTML
+    text = marked(text);
+    // add content to template
+    html = html.replace('CONTENTGOESHERE', text);    
+    return [true, html];
+  }
+  catch (e) {
+    // console.log(e);
+    console.log('404');
+    text = marked("# 404\n\nSorry, couldn't find what you're looking for.");
+    html = html.replace('CONTENTGOESHERE', text);
+    console.log(html);
+    return [false, html];    
+  }   
+  
 }
 
 
 // get request to /test -> index page
-app.get('/test', (req,res) => {  
-  res.status(200);  
+app.get('/*', (req,res) => {  
   // build page
-  let page = get_page('index');
-  res.send(page);
+  //let page = get_page('index');  
+  // res.status(200);  
+  // res.send(page);    
+
+  if (req.url == '/') {
+    let index = get_page('index');  
+    res.status(200);  
+    res.send(index[1]);
+  } 
+  else {    
+    let page = get_page(req.url);
+    if (page[0]) {
+      res.status(200);
+    } else {
+      res.status(404);
+    }
+    res.send(page[1]);
+  }
+
+
 });
 
 // start server
