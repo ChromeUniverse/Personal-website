c10b1be1a152c328a7cc599bad3f02e7bab4caf7
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-08-19 15:51:14 -0300

    Updated Express webserver with favicon middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // express.js setup
 const express = require("express");
+const favicon = require("serve-favicon")
+const path = require('path')
+
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // filesystem
 const fs = require("fs");
@@ -10,12 +14,13 @@ const fs = require("fs");
 // extra consts
 const path_to_htmls = __dirname + '/public/htmls/'
 
-// get request to /test -> index page
+// routes 
 app.get('/*', (req,res) => {  
   console.log(req.url);
 
   let req_endpoint = req.url.substr(1);
 
+  // get request to / -> serve index page
   if (req.url == '/') {
     let page = fs.readFileSync(path_to_htmls + 'index.html', 'UTF-8').toString();
     res.status(200);
