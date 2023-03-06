843313804145fa920fb3f335356167ad80d6a6f2
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2023-03-06 16:18:17 -0300

    Updated configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,14 @@ import react from "@astrojs/react";
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
+import remarkToc from "remark-toc";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   site: "https://blaring.net",
+  markdown: {
+    // remarkPlugins: [remarkToc],
+    // syntaxHighlight: "prism",
+  },
 });
